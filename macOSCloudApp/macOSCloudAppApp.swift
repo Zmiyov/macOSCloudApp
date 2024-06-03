b1668624ef5c72ecaf30451095e151b5e763a423
Volodymyr <zmiyov88@gmail.com>
2024-06-03 18:46:54 +0300

    Initial Commit

---
+++ macOSCloudApp/macOSCloudAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSCloudAppApp.swift
+//  macOSCloudApp
+//
+//  Created by Vladimir Pisarenko on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct macOSCloudAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
